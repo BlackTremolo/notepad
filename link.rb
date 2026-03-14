9518ab93b1f97d23baf220100ead4d0d33f8b14e
BlackTremolo <mihsee436l@gmail.com>
2026-03-14 21:49:37 +0800

    modificate childs classes

--- link.rb
+++ link.rb
@@ -1,14 +1,19 @@
-class Limk < Post
+class Link < Post
   def initialize
     super
     @url = nil
   end
 
   def read_from_console
+    puts 'Введите адрес ссылки'
+    @url = STDIN.gets.strip
 
+    puts 'Введите описание'
+    @text = STDIN.gets.strip
   end
 
   def to_strings
-
+    time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')}\n"
+    return [@url, @text, time_string]
   end
 end
--- task.rb
+++ task.rb
@@ -2,15 +2,22 @@ require 'date'
 class Task < Post
   def initialize
     super
-
     @dui_date = Time.now
   end
 
   def read_from_console
+    puts 'Что нужно сделать?'
+    @text = STDIN.gets.strip
 
+    puts 'До какого числа нужно это сделать'
+    input = STDIN.gets.strip
+
+    @dui_date = Date.parse(input)
   end
 
   def to_strings
-
+    time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')}\n"
+    deadline = "Крайний срок: #{@dui_date}"
+    return [deadline, @text, time_string]
   end
 end
