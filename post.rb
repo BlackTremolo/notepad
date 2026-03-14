2662d1f6c55d8c08f0a97eede6dbe765fd25a8d5
BlackTremolo <mihsee436l@gmail.com>
2026-03-14 19:12:00 +0800

    Add parent class Post

---
+++ post.rb
@@ -0,0 +1,21 @@
+class Post
+  def initialize
+    @text = nil
+    @created_at = Time.now
+  end
+  #считывание данных из консоли в нужные поля
+  def read_from_console
+
+  end
+
+  #запись данных в массив строк
+  def to_strings
+
+  end
+
+  #сохранение в файл
+  def save
+
+  end
+
+end
