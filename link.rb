405f54b0ea9b51cd055f8e2d825caa7a5f7ccb88
BlackTremolo <mihsee436l@gmail.com>
2026-03-14 19:24:18 +0800

    Add childs classes

---
+++ link.rb
@@ -0,0 +1,14 @@
+class Limk < Post
+  def initialize
+    super
+    @url = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+end
---
+++ task.rb
@@ -0,0 +1,16 @@
+require 'date'
+class Task < Post
+  def initialize
+    super
+
+    @dui_date = Time.now
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+end
